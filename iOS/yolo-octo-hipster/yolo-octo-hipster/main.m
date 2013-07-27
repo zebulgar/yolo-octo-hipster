bec97e139c718217717f51d290539eaa74d3d1f6
Zachary Hamed <zhamed@college.harvard.edu>
2013-07-27 13:48:03 -0700

    working accelerometer display

---
+++ iOS/yolo-octo-hipster/yolo-octo-hipster/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  yolo-octo-hipster
+//
+//  Created by Zach Hamed on 7/27/13.
+//  Copyright (c) 2013 Zach Hamed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
