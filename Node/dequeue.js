1bd3c3b40d81caf48a3b0dfed43d344f5a7684f5
Eric Bakan <eric@ebakan.com>
2013-07-27 15:49:37 -0700

    Added dequeue.js

---
+++ Node/dequeue.js
@@ -0,0 +1,74 @@
+// http://david-burger.blogspot.com/2009/07/javascript-dequeue.html
+var Dequeue = function() {
+  this._front = new Dequeue.node();
+  this._back = new Dequeue.node();
+  this._front._prev = this._back;
+  this._back._next = this._front;
+}
+
+Dequeue.node = function(data) {
+  this._data = data;
+  this._prev = null;
+  this._next = null;
+};
+ 
+Dequeue.prototype.empty = function() {
+  return this._front._prev === this._back;
+};
+ 
+Dequeue.prototype.push = function(data) {
+  if (data) {
+    var node = new Dequeue.Node(data);
+    node._prev = this._front;
+    this._front._next = node;
+    this._front = node;
+  }
+};
+ 
+Dequeue.prototype.pop_front = function() {
+  if (this.empty()) {
+    throw new Error("pop_front() called on empty dequeue");
+  } else {
+    var node = this._front;
+    this._front = node._prev;
+    this._front._next = null;
+    return node._data;
+  }
+};
+
+Dequeue.prototype.pop_back = function() {
+  if (this.empty()) {
+    throw new Error("pop_back() called on empty dequeue");
+  } else {
+    var node = this._back;
+    this._back = node._next;
+    this._back._prev = null;
+    return node._data;
+  }
+};
+
+Dequeue.prototype.peek_front = function() {
+  if (this.empty()) {
+    return null;
+  }
+  return this._front._data;
+};
+
+Dequeue.prototype.peek_back = function() {
+  if (this.empty()) {
+    return null;
+  }
+  return this._back._data;
+};
+
+Dequeue.prototype.to_array = function() {
+  var out = []
+  node = this._front;
+  while (node) {
+    out.push(node)
+    node = node._prev;
+  }
+  return out;
+}
+
+module.exports = Dequeue;
