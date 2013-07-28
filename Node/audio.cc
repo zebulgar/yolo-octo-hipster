7e6c2119f80ed7fedd0be01a76b55750dea3d843
Eric Bakan <eric@ebakan.com>
2013-07-27 20:33:11 -0700

    Sped up audio.cc

--- Node/audio.cc
+++ Node/audio.cc
@@ -5,12 +5,12 @@
 using namespace v8;
 
 AudioDeviceID outputDeviceID = kAudioObjectUnknown;
+UInt32 propertySize = 0;
+OSStatus status = noErr;
+AudioObjectPropertyAddress propertyAOPA;
+Float32 volumeToSet;
 
 void foo() {
-  UInt32 propertySize = 0;
-  OSStatus status = noErr;
-  AudioObjectPropertyAddress propertyAOPA;
-
   propertyAOPA.mElement = kAudioObjectPropertyElementMaster;
   propertyAOPA.mScope = kAudioObjectPropertyScopeGlobal;
   propertyAOPA.mSelector = kAudioHardwarePropertyDefaultOutputDevice;
@@ -18,38 +18,20 @@ void foo() {
 
   status = AudioHardwareServiceGetPropertyData(kAudioObjectSystemObject, &propertyAOPA, 0, NULL, &propertySize, &outputDeviceID); 
 
-  if(status) {
-    // Error
-    return;
-  }
-}
-
-void bar(double vol) {
-  Float32 newVolume = vol;
-  UInt32 propertySize = 0;
-  OSStatus status = noErr;
-  AudioObjectPropertyAddress propertyAOPA;
-
-  foo();
-
   propertyAOPA.mElement = kAudioObjectPropertyElementMaster;
   propertyAOPA.mScope = kAudioDevicePropertyScopeOutput;
   propertyAOPA.mSelector = kAudioHardwareServiceDeviceProperty_VirtualMasterVolume;
-  Float32 volumeToSet;
   propertySize = sizeof(Float32);
 
-  if (newVolume > 1.0)
+}
+
+void bar(double vol) {
+  if (vol > 1.0)
     volumeToSet = 1.0;
   else
-    volumeToSet = newVolume;
+    volumeToSet = vol;
 
-  status = AudioHardwareServiceSetPropertyData(outputDeviceID, &propertyAOPA, 0, NULL, propertySize, &newVolume);   
-  printf("checkstatus: %d\n",status);
-
-  if(status) {
-    // Error
-    return;
-  }
+  AudioHardwareServiceSetPropertyData(outputDeviceID, &propertyAOPA, 0, NULL, propertySize, &volumeToSet);   
 }
 
 Handle<Value> SetVol(const Arguments& args) {
@@ -70,6 +52,7 @@ Handle<Value> SetVol(const Arguments& args) {
 }
 
 void Init(Handle<Object> exports) {
+  foo();
   exports->Set(String::NewSymbol("setVol"),
       FunctionTemplate::New(SetVol)->GetFunction());
 }
